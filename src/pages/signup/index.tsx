18c5db721d4ce719d0993e80b079488247630fb6
shingotakagij7a <shingo.takagi.j7a@jp.denso.com>
2023-03-17 13:30:26 +0900

    add sign up page template

---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,65 @@
+import {
+  Box,
+  Button,
+  Center,
+  chakra,
+  Container,
+  FormControl,
+  FormLabel,
+  Grid,
+  Heading,
+  Input,
+  Spacer
+} from '@chakra-ui/react';
+import { FormEvent, useState } from 'react';
+
+export const SignUp = () => {
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    console.log({ email, password });
+    e.preventDefault()
+  }
+
+  return (
+    <Container py={14}>
+      <Heading>サインアップ</Heading>
+      <chakra.form onSubmit={handleSubmit}>
+        <Spacer height={8} aria-hidden />
+        <Grid gap={4}>
+          <Box display={'contents'}>
+            <FormControl>
+              <FormLabel>メールアドレス</FormLabel>
+              <Input
+                type={'email'}
+                name={'email'}
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value)
+                }}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>パスワード</FormLabel>
+              <Input
+                type={'password'}
+                name={'password'}
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value)
+                }}
+              />
+            </FormControl>
+          </Box>
+        </Grid>
+        <Spacer height={4} aria-hidden />
+        <Center>
+          <Button type={'submit'}>アカウントを作成</Button>
+        </Center>
+      </chakra.form>
+    </Container>
+  )
+}
+
+export default SignUp
