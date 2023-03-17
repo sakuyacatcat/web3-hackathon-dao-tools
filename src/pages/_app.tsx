b096bb55bd8a15063163947172193ada933c4e59
shingotakagij7a <shingo.takagi.j7a@jp.denso.com>
2023-03-17 11:01:01 +0900

    add firebase setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import { initializeFirebaseApp } from '@src/configs/initFirebase'
+import { getApp } from 'firebase/app'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+initializeFirebaseApp()
+function App({ Component, pageProps }: AppProps) {
+  console.log(getApp())
   return (
     <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
   )
 }
+
+export default App
