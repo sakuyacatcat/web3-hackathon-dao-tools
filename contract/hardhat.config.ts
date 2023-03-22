0e15a2cc6bda3f9e727f089f36e5865b212d53af
shingotakagij7a <shingo.takagi.j7a@jp.denso.com>
2023-03-22 21:15:52 +0900

    update contract dir template

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,8 +1,41 @@
-import '@nomicfoundation/hardhat-toolbox'
-import { HardhatUserConfig } from 'hardhat/config'
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
+import { HardhatUserConfig } from 'hardhat/config';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.18',
-}
+  solidity: {
+    version: '0.8.17',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 20,
+      },
+    },
+  },
+  networks: {
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [process.env.WALLET_PRIVATE_KEY!].filter(Boolean),
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: process.env.MNEMONIC
+        ? { mnemonic: process.env.MNEMONIC }
+        : [process.env.WALLET_PRIVATE_KEY!].filter(Boolean),
+    },
+    localhost: {
+      chainId: 1337,
+      initialBaseFeePerGas: 0,
+    },
+  },
+  mocha: {
+    timeout: 60_000,
+  },
+};
 
-export default config
+export default config;
