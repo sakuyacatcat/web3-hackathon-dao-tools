a401ad77340bfcf7ca3be25c7dafe4a446d7fc34
shingotakagij7a <shingo.takagi.j7a@jp.denso.com>
2023-03-23 13:22:56 +0900

    rename deploy scripts

--- contract/scripts/deploy_testnet.ts
+++ contract/scripts/deploy_testnet.ts
@@ -9,12 +9,17 @@ async function main() {
   const totalSupply = "10000000000000000000000000000";
 
   const MyToken = await ethers.getContractFactory("MyToken");
-  const myToken = await MyToken.deploy(totalSupply);
+  const myToken = await MyToken.deploy("JapanYenCoin", "JPYC", totalSupply);
+  const InvestByVote = await ethers.getContractFactory("InvestByVote");
+  const investByVote = await InvestByVote.deploy(myToken.address);
 
-  await myToken.approve(deployer.address, totalSupply);
-  await myToken.transfer(deployer.address, "10000000000000000000000000000");
-
+  console.log("Contract deployed, InvestByVote address:", investByVote.address);
   console.log("Contract deployed, MyToken(DummyJPYC) address:", myToken.address);
+
+  await myToken.approve(investByVote.address, totalSupply);
+  await myToken.transfer(investByVote.address, "10000000000000000000000000000");
+
+  console.log("Complete transfer all JPYC to InvestByVote address");
 }
 
 main()
